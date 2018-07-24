b2458f653458ca46a1fdefece3c2f6f806ab5b83
tacodev <tacodev@skt-bluejaymbpui-MacBook-Pro.local>
2018-07-24 18:12:42 +0900

    fir

---
+++ helpy.py
@@ -0,0 +1,11 @@
+print ("*"*30)
+
+for i in range (5,10) : 
+    print ('Hello Jenkins'.center(10+2*i))
+
+for i in range (10,5,-1) :
+    print ('Jenkins Hello'.center(10+2*i))
+
+print ("*"*30)
+
+
