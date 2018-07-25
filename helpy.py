c81dc8951a396d3d2ecf4c82cdbd15ea3025e957
tacodev <tacodev@skt-bluejaymbpui-MacBook-Pro.local>
2018-07-25 17:40:15 +0900

    third

--- helpy.py
+++ helpy.py
@@ -1,13 +1,6 @@
 print ("*"*12+"start "+"*"*12)
-print ("*"*30)
 
-
-for i in range (5,10) : 
-    print ('Hello Jenkins'.center(10+2*i))
-
-for i in range (10,5,-1) :
-    print ('Jenkins Hello'.center(10+2*i))
-
+print ( "go JENKINS! ")
 print ("*"*30)
 
 
